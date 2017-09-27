2bebbd0e2a8aae2dc16aaa33fb39b20d8ee1eb3d
vascomramalho <me@bradtraversy.com>
2017-09-27 15:43:48 +0100

    all files uploaded

---
+++ projects_text.js
@@ -0,0 +1,66 @@
+const project_texts = {
+  /*WRITE HERE PROJECT*/
+  'Plasma Physics Research': 
+  /*WRITE HERE PROJECT SUBTITLE*/
+  ["Study of Magnetic Confinement of Plasma Rings",
+  /*WRITE HERE PROJECT TEXT*/
+  "Between September 2015 and July 2016 I did some research on Plasma Physics, at Leiden University. This project consisted in the attempt to visualize the theoretical prediction of magnetic fields in plasma with toroidal shapes. The studied plasma was a Laser Induced Plasma on a Helium chamber. For this I had to use multiple visualisation techniques such as Faraday Rotation, Interferometry and Spectroscopy. Through out this study many other questions raised regarding the origin itself of these rings, which provided the material for a study of the hydrodynamical effects of the initial stage of the laser induced plasma.During this project I developed many skills on electronics, programming, image analysis and abstract mathematics."],
+  
+   /*WRITE HERE PROJECT*/
+  'Analytic Mechanics Studies': 
+  /*WRITE HERE PROJECT SUBTITLE*/
+  ["Numerical Simulation of Chain Fountain",
+  /*WRITE HERE PROJECT TEXT*/
+    `In 2014 I worked for 6 moths as a part time student 
+    worker in the Leonardo Da Vinci Center for Bionics, in
+    the Technical University of Munich. I did some research
+    for a project that intended to explain the Chain Fountain 
+    phenomena – this is an interesting mechanical phenomena that
+    happens when the tail of a beam chain is thrown out of a 
+    bucket and its fall creates a lift in the remaining chain 
+    body. Due to its strange layout and its possible applications
+    in aerospace engineering, I was commissioned a numerical study
+    to confirm the theoretical mechanical approach. For this I had 
+    to study some numerical methods and multi-body simulation methods.Also
+    , in parallel I developed an interpretation using the Fluid Mechanics
+    approach, treating the chain as running fluid pipe.It was a very 
+    insightful work that allowed me to develop some insight on numerical 
+    methods and its challenges.`],
+
+     /*WRITE HERE PROJECT*/
+  'Wave Synthesiser': 
+  /*WRITE HERE PROJECT SUBTITLE*/
+  ["Design of a Wave Synthesiser for a Laser Pulse.",
+  /*WRITE HERE PROJECT TEXT*/
+    `In 2015 I work as an assistant student at the Max Planck 
+    Institute for Quantum Optics, in Munich. My work was to design 
+    an efficient layout for a phase matching synthesiser. To produce
+    an ultra short pulse, a continuous frequency spectrum was achieved,
+    and to match the out coming wave phase, the spectrum was divided into
+    two beams that would go through different phase delay channels. At the 
+    end both channels would be combined to achieve the final pulse, combined
+    both in space and time. This work was done under the supervision of Dr. Martin Shultze.`],
+
+'Os Capitães da Areia': 
+  /*WRITE HERE PROJECT SUBTITLE*/
+  ["Composer and Musician for a Pop Band ",
+  /*WRITE HERE PROJECT TEXT*/
+    `Founder of the Portuguese pop band – Os Capitães da Areia. Band inspired in the 80’s sounds with a touch of summer and jovial attitude. Founded in 2009, with the release of the first album in 2011 – “O Verão Eterno dos Capitães 
+    da Areia” and the second Album in 2015 – 
+    “A Viagem dos Capitães da Areia a bordo do 
+    Apolo 70”`],
+
+
+
+    };
+
+
+
+
+
+
+
+
+
+
+
